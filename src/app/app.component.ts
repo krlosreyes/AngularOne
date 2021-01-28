a8e98e434cb3a34037f44eea16be733d8f2cd8dc
Charlie <caxireit@gmail.com>
2021-01-28 17:13:30 -0500

    mi primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularOne';
+  users = ['Charlie', 'Sony', 'Paula', 'Sara', 'Jesus']
 }
